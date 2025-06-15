87dde1fa77625328d044a979682fe2d0a449f723
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-15 16:19:38 +0900

    で変数、定数にまとめた

--- app.js
+++ app.js
@@ -14,24 +14,21 @@ const correctAnswer = 'ゴムゴムの実'
 //定数の文字列をHTMLに反映　
 document.getElementById('js-question').textContent = quiz;
 
+const $button = document.getElementsByTagName('button');
+
+$button[0].textContent = answer[0];
+$button[1].textContent = answer[1];
+$button[2].textContent = answer[2];
+$button[3].textContent = answer[3];
+$button[4].textContent = answer[4];
+$button[5].textContent = answer[5];
+
 //同じような処理が続いたので、変数、定数にまとめる
 //$があることで、HTMLのオブジェクトが入っていると理解できる
-document.getElementsByTagName('button')[0].textContent = answer[0];
-document.getElementsByTagName('button')[1].textContent = answer[1];
-document.getElementsByTagName('button')[2].textContent = answer[2];
-document.getElementsByTagName('button')[3].textContent = answer[3];
-document.getElementsByTagName('button')[4].textContent = answer[4];
-document.getElementsByTagName('button')[5].textContent = answer[5];
-
-// const $button = document.getElementsByTagName('button');
-
-
-
-
 
 //ボタンを押したら正誤判定できる
-document.getElementsByTagName('button')[0].addEventListener('click', () => {
-    if (correctAnswer === document.getElementsByTagName('button')[0].textContent) {
+$button[0].addEventListener('click', () => {
+    if (correctAnswer === $button[0].textContent) {
     window.alert('正解!');
     } else {
     window.alert('不正解！');
