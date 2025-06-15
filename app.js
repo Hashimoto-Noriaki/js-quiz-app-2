3c53aa38e56818e9b9e33e794c4b7cd6f8fc29ca
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-15 18:36:21 +0900

    2

--- app.js
+++ app.js
@@ -6,10 +6,37 @@ const answer = [
     'バラバラの実',
 ]
 
-const correctAnser = 'ゴムゴムの実'
+const correctAnswer = 'ゴムゴムの実'
 
 document.getElementById('js-question').textContent = quiz;
 
-document.getElementsByTagName('button')[0].textContent = answer[0];
-document.getElementsByTagName('button')[1].textContent = answer[1];
-document.getElementsByTagName('button')[2].textContent = answer[2];
+const $button = document.getElementsByTagName('button');
+
+$button[0].textContent = answer[0];
+$button[1].textContent = answer[1];
+$button[2].textContent = answer[2];
+
+//正誤判定
+$button[0].addEventListener('click',()=> {
+    if(correctAnswer === document.getElementsByTagName('button')[0].textContent){
+        alert("正解!")
+    } else {
+        alert("不正解!")
+    }
+})
+
+$button[1].addEventListener('click',()=> {
+    if(correctAnswer === document.getElementsByTagName('button')[1].textContent){
+        alert("正解!")
+    } else {
+        alert("不正解!")
+    }
+})
+
+$button[2].addEventListener('click',()=> {
+    if(correctAnswer === document.getElementsByTagName('button')[2].textContent){
+        alert("正解!")
+    } else {
+        alert("不正解!")
+    }
+})
