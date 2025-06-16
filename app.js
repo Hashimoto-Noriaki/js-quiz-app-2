6ae022c58a7e214dd1946943a98026ab01afe8e8
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-16 16:44:06 +0900

    whileで囲む

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ $button[0].textContent = answers[0];
 $button[1].textContent = answers[1];
 $button[2].textContent = answers[2];
 
+let buttonLength = $button.length
+let buttonIndex = 0;
+while(buttonIndex < buttonLength){
+    $button[buttonIndex].textContent = answers[buttonIndex];
+    buttonIndex++;
+}
+
 $button[0].addEventListener('click',()=> {
     if(correct === $button[0].textContent){
         alert('正解');
