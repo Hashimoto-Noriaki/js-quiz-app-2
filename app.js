941e8382d6e4383364b3fc6e9991cef9f9d05f5c
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-16 16:19:10 +0900

    正誤問題作成

--- app.js
+++ app.js
@@ -8,7 +8,33 @@ const answers = [
 
 const correct = 'モンキー・D・ルフィ';
 
+const $button = document.getElementsByTagName('button');
+
 document.getElementById('js-question').textContent = question;
-document.getElementsByTagName('button')[0].textContent = answers[0];
-document.getElementsByTagName('button')[1].textContent = answers[1];
-document.getElementsByTagName('button')[2].textContent = answers[2];
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+
+$button[0].addEventListener('click',()=> {
+    if(correct === $button[0].textContent){
+        alert('正解');
+    } else {
+        alert('不正解');
+    }
+})
+
+$button[1].addEventListener('click',()=> {
+    if(correct === $button[1].textContent){
+        alert('正解');
+    } else {
+        alert('不正解');
+    }
+})
+
+$button[2].addEventListener('click',()=> {
+    if(correct === $button[2].textContent){
+        alert('正解');
+    } else {
+        alert('不正解');
+    }
+})
