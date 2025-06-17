482e45a36ba2fda4997dfbb10a58f94f768e41ff
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-17 23:46:31 +0900

    完成

--- app.js
+++ app.js
@@ -1,21 +1,54 @@
-const question = 'ワンピースの主人公の名前は何ですか？';
+const quiz = [
+  {
+    question: 'ルフィの悪魔は？',
+    answers: [
+      'ゴムゴムの実',
+      'バラバラの実',
+      'メラメラの実',
+      'ピカピカの実',
+      'ハナハナの実',
+      'ゴロゴロの実',
+    ],
+    correct: 'ゴムゴムの実',
+  },
+  {
+    question: '桜木花道の出身中学は?',
+    answers: [
+      '北村中',
+      '和光中',
+      '武石中',
+      '富ケ丘中',
+      '大岡中',
+      '竹中中',
+    ],
+    correct: '和光中',
+  },
+  {
+    question: '1998年に64で発売されたゼルダの伝説のゲームのタイトル名は?',
+    answers: [
+      'ムジュラの仮面',
+      'トワイライトプリンセス',
+      '時のオカリナ',
+      '風のタクト',
+      'スカイウォードソード',
+      '神々のトライフォース',
+    ],
+    correct: '時のオカリナ',
+  },
+];
 
-const answers = [
-    'ルフィ',
-    'ゾロ',
-    'ナミ'
-]
-
-const correct = 'ルフィ';
+let quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
 
 const $button = document.getElementsByTagName('button')
 let buttonLength = $button.length
 
 const setupQuiz = () => {
-    document.getElementById('js-question').textContent = question;
+    document.getElementById('js-question').textContent = quiz[quizIndex].question;
     let buttonIndex = 0;
     while(buttonIndex < buttonLength){
-        $button[buttonIndex].textContent = answers[buttonIndex];
+        $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
         buttonIndex++;
     }
 }
@@ -23,10 +56,20 @@ const setupQuiz = () => {
 setupQuiz();
 
 const clickHandler = (e)=> {
-    if(correct === e.target.textContent){
+    if(quiz[quizIndex].correct === e.target.textContent){
         alert('正解！');
+        score++;
     } else {
         alert('不正解！');
+    }
+
+    quizIndex++;
+    if(quizIndex < quizLength){
+        setupQuiz();
+    } else{
+        alert('クイズ終了! 正解数は' + score + '/' + quizLength + 'です。');
+        quizIndex= 0;
+        setupQuiz();
     }
 }
 
